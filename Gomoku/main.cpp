a7254afb929554ceb6bb9cf19e0bed3a68d28833
nervonment <consexe@outlook.com>
2023-03-12 22:28:45 +0800

    没有更改

--- Gomoku/main.cpp
+++ Gomoku/main.cpp
@@ -3,12 +3,6 @@
 int main() {
 	Game game;
 
-	//for (int i = 0; i < 15; ++i) {
-	//	for (int j = 0; j < 15; ++j)
-	//		std::cout << game.point_weight(i, j) << ", ";
-	//	std::cout << "\n";
-	//}
-
 	auto hwnd = GetHWnd();
 	while (IsWindow(hwnd)) {
 		game.Draw();
